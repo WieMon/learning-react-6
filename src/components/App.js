7002696ddfc2d32df47f2f986951fe9f4b01f323
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-14 11:53:37 +0200

    Add a new url

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-           <li><a href='/'>Start</a></li>
-           <li><a href='/news'>News</a></li>
-           <li><a href='/contact'>Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-      <section>
-        Hello
-      </section>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <nav>
+            <ul>
+            {/* <li><a href='/'>Start</a></li>
+            <li><a href='/news'>News</a></li>
+            <li><a href='/contact'>Contact</a></li> */}
+            <li><Link to='/'>Start</Link></li>
+            <li><Link to='/news'>News</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          Hello
+        </section>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
